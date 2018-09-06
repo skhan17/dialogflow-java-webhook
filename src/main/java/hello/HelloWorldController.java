394188b682bc98a8f5164a428f7712bec53be730
skhan17 <shyaankhan55@gmail.com>
2018-09-06 16:33:38 -0700

    combined 2 controllers to one class

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 import org.json.*;
 
 @Controller
-@RequestMapping("/webhook")
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value ="/webhook", method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
 
         System.out.println(obj);
@@ -19,5 +19,12 @@ public class HelloWorldController {
         JSONObject json = new JSONObject(obj);
 
         return new WebhookResponse("Hello! " + obj, "Text " + obj);
+    }
+    @RequestMapping(value ="/", method = RequestMethod.GET)
+    public @ResponseBody ModelAndView mainPage(@RequestBody String obj){
+
+    	System.out.println("Yo YO YO");
+
+        return new ModelAndView("HELLO");
     }
 }
