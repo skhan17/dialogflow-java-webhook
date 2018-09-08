3d362f6e67f65fbfa8694ee21c97eef0944c6f46
skhan17 <shyaankhan55@gmail.com>
2018-09-08 12:29:11 -0700

    api functionality

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -14,7 +14,7 @@ import java.net.*;
 
 
 @RestController
-public class HelloWorldController {
+public class HelloWorldController throws MalformedURLException, IOException {
 	public String firstName;
     public String lastName;
    	public String airlines;	
@@ -47,19 +47,32 @@ public class HelloWorldController {
         		ticket.setAirlines(airlines);
         		ticket.setNumBags(numOfBags);
 
-        		StringBuilder bufferedString = new StringBuilder();	
-
         		String urlString = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/JBU/1201/dep/2018/9/8?appId=d234e79a&appKey=af4ce6fc3c6761042323676428a7d396&utc=false";
         		URL url = new URL(urlString);
         		URLConnection conn = url.openConnection();
-        		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			    String line;
-			    while ((line = rd.readLine()) != null) {
-			       	bufferedString.append(line);
-			    }
-			    rd.close();
+        		
 
-      			System.out.print(bufferedString.toString());
+        		BufferedReader br = null;
+				StringBuilder sb = new StringBuilder();
+				String line;
+
+        		try{
+        			br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					while ((line = br.readLine()) != null) {
+						sb.append(line);
+				}
+        		catch(MalformedURLException ml, IOException ex){
+        			ex.printStackTrace();
+        		}finally{
+        			if (br != null) {
+						try {
+							br.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+        		}
+      			System.out.print(sb.toString());
         		break;
         	}
         }
